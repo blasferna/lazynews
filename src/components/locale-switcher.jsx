1b9651dea15ef68e777b3c2d75f020c0083fa9b6
blasferna <blasferna@gmail.com>
2024-07-21 16:49:42 -0400

    feat: Add @radix-ui/react-dropdown-menu dependency and update locale switcher component

--- src/components/locale-switcher.jsx
+++ src/components/locale-switcher.jsx
@@ -1,6 +1,7 @@
-import { cn } from "@/lib/utils";
 import { useLocale, useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
 
 export default function LocaleSwitcher() {
   const router = useRouter();
@@ -12,25 +13,46 @@ export default function LocaleSwitcher() {
   };
 
   return (
-    <div className="locale-switcher flex items-center gap-2">
-      <button
-        onClick={() => changeLocale("en")}
-        className={cn(
-          locale === "en" && "font-bold",
-          "hover:underline focus:outline-none focus:ring-2 focus:ring-primary"
-        )}
-      >
-        {t("english")}
-      </button>
-      <button
-        onClick={() => changeLocale("es")}
-        className={cn(
-            locale === "es" && "font-bold",
-            "hover:underline focus:outline-none focus:ring-2 focus:ring-primary"
-          )}
-        >
-        {t("spanish")}
-      </button>
-    </div>
-  );
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" className="flex items-center gap-2">
+          <span className="">{locale === "es" ? t("spanish") : t("english")}</span>
+          <ChevronsUpDownIcon className="ml-2 h-4 w-4 opacity-50" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[130px]">
+        <DropdownMenuItem onClick={() => changeLocale("es")}>
+          <div className="flex items-center gap-2">
+            <span>{t("spanish")}</span>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => changeLocale("en")}>
+          <div className="flex items-center gap-2">
+            <span>{t("english")}</span>
+          </div>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+
+function ChevronsUpDownIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m7 15 5 5 5-5" />
+      <path d="m7 9 5-5 5 5" />
+    </svg>
+  )
 }
