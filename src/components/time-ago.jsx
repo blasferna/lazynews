046a7a216a4ef8508c5600629d09b7d6b5e4fd2a
blasferna <blasferna@gmail.com>
2024-07-22 16:24:57 -0400

    fix: locale

--- src/components/time-ago.jsx
+++ src/components/time-ago.jsx
@@ -1,11 +1,11 @@
 "use client";
 import { formatDistanceToNow } from "date-fns";
-import { en, enUS } from "date-fns/locale";
+import { enUS, es } from "date-fns/locale";
 import { useLocale } from "next-intl";
 
 const TimeAgo = ({ date }) => {
   const localeName = useLocale();
-  const locale = localeName === "en" ? en : enUS;
+  const locale = localeName === "en" ? enUS : es;
   const timeAgo = formatDistanceToNow(new Date(date), { addSuffix: true, locale});
   return <span className="article-time-ago">{timeAgo}</span>;
 };
