b2e14b97c647d2b1d143c056878734c059e369ab
blasferna <blasferna@gmail.com>
2024-07-22 16:04:04 -0400

    refactor: Update locale switcher component to use enUS locale for date-fns

--- src/components/time-ago.jsx
+++ src/components/time-ago.jsx
@@ -1,11 +1,11 @@
 "use client";
 import { formatDistanceToNow } from "date-fns";
-import { en, es } from "date-fns/locale";
+import { en, enUS } from "date-fns/locale";
 import { useLocale } from "next-intl";
 
 const TimeAgo = ({ date }) => {
   const localeName = useLocale();
-  const locale = localeName === "en" ? en : es;
+  const locale = localeName === "en" ? en : enUS;
   const timeAgo = formatDistanceToNow(new Date(date), { addSuffix: true, locale});
   return <span className="article-time-ago">{timeAgo}</span>;
 };
