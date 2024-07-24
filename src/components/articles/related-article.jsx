e781bc7730966571c5d963883d14b6fc9fa490aa
blasferna <blasferna@gmail.com>
2024-07-24 11:22:27 -0400

    refactor: Update related-article component to use nextjs link component

--- src/components/articles/related-article.jsx
+++ src/components/articles/related-article.jsx
@@ -1,36 +1,31 @@
-"use client";
 import { truncateTitle } from "@/lib/utils";
-import { useLocale } from "next-intl";
-import { useRouter } from "next/navigation";
+import { getLocale } from "next-intl/server";
+import Link from "next/link";
 
-const RelatedArticle = ({ article }) => {
-  const router = useRouter();
-  const locale = useLocale();
+const RelatedArticle = async ({ article }) => {
+  const locale = await getLocale();
 
   const getTruncatedTitle = (title) => {
     return truncateTitle(title, 70);
   };
 
   return (
-    <div
-      className="flex items-center gap-2 mb-3 group hover:cursor-pointer"
-      onClick={() =>
-        router.push(
-          `/${locale}/${article.section}/${encodeURIComponent(article.url)}`
-        )
-      }
+    <Link
+      href={`/${locale}/${article.section}/${encodeURIComponent(article.url)}`}
     >
-      <img
-        src={article.image}
-        alt="Article Thumbnail"
-        className="rounded-md w-[130px] h-[73px] object-contain aspect-video group-hover:brightness-75 article-image"
-      />
-      <div>
-        <h3 className="text-lg group-hover:underline" title={article.title}>
-          {getTruncatedTitle(article.title)}
-        </h3>
+      <div className="flex items-center gap-2 mb-3 group hover:cursor-pointer">
+        <img
+          src={article.image}
+          alt="Article Thumbnail"
+          className="rounded-md w-[130px] h-[73px] object-contain aspect-video group-hover:brightness-75 article-image"
+        />
+        <div>
+          <h3 className="text-lg group-hover:underline" title={article.title}>
+            {getTruncatedTitle(article.title)}
+          </h3>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
